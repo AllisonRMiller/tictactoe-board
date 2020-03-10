931677be1b9f70814da5871561d81e048e1d7ccc
AllisonRMiller <60373159+AllisonRMiller@users.noreply.github.com>
2020-03-10 16:36:58 -0400

    afternoon commit

---
+++ main.js
@@ -0,0 +1,92 @@
+let state=0
+
+
+// Object: div
+// object: 
+
+
+
+
+// Draw interface
+    // Draw splash page
+        // Make container
+            const newDiv = document.createElement.bind(document);
+            container = newDiv("div");
+            // make row
+            row1 = newDiv("div");
+            // make column
+            column1 = NewDiv("div");
+            // define container
+                container.classList = "container";
+            // define row
+                row1.classList = "row";
+            // define column
+                column1.classList = "col";
+            // append column to row
+                row1.appendChild(column1);
+            // append row to container
+                container.appendChild(row1);
+            // append container to body
+                document.body.appendChild(container);
+        if (state=0){
+        // make title
+            // jumbotrown goes here
+        // make button
+        }
+        // define title
+        // define button
+        // append title to column
+        // append button to column
+    // draw input page
+    if (state=2){
+        // Draw p1 label
+            player1 = newDiv("h2")
+            // Draw p1 input box
+
+        // Draw p2 label
+            player2 = newDiv("h2");
+            // draw p2 input box
+
+        // Draw submit button
+    }
+    // Draw header
+        // make header
+        // define header
+        // append row to container
+    // draw board
+        // Draw one div
+            // Make board container
+            // make row
+            // make column
+            // define column
+            // append column three times
+            // define row
+            // append row three times
+    // draw player 1 and player 2
+        // Draw player 1
+            // Draw player 1 name
+            // Draw which symbol player 1 is
+            // Draw score
+            // Active indicator
+        // Draw player 2
+            // Draw player 2 name
+            // Draw which symbol player 2 is
+            // Draw score
+            // active indicator
+    // draw button
+        // draw button text
+
+// Map board
+    // Define each square
+// Create player object
+    // Create player 1
+    // Create player 2
+// Check turn --this is a binary choice, use true/false?
+// Reset button function
+// Draw mark on board --tie to check turn?
+// Change active player
+// Advance state
+// Check win
+// Board click function
+// click listener for board
+// click listener for reset
