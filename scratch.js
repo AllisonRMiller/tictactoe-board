662fbf1fd97d419a26e248f72e03dd857b11f71f
AllisonRMiller <60373159+AllisonRMiller@users.noreply.github.com>
2020-03-15 22:45:16 -0400

    var dec
    Minor cleanup

--- scratch.js
+++ scratch.js
@@ -79,7 +79,7 @@ function buildPage() {
     console.log(container2);
 
 
-    for (i = 0; i < 3; i++) {
+    for (var i = 0; i < 3; i++) {
         let gridRow = new Element("div", "row", "gridRow" + i);
         console.log(gridRow);
         for (j = 0; j < 3; j++) {
